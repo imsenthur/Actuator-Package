bdad0d2bee027e0c6c86c206ddb484f447ed435d
JedCDephy <jcummings@dephy.com>
2020-12-09 17:26:21 -0500

    ank ang check added

--- Python/flexseapython/flexsea_demo/eb60_int_functional_test.py
+++ Python/flexseapython/flexsea_demo/eb60_int_functional_test.py
@@ -17,15 +17,16 @@ def fxEB60IntFunctTest(port, baudRate, time = 2, num_times = 5,
 
 	testFlag = True
 	if testFlag:
-		testFlag = eb60SensorCheck(devId, appType, time=6, time_step=0.1)
+		testFlag = eb60SensorCheck(devId, appType, time=2, time_step=0.1)
 	if testFlag:
-		testFlag = eb60AnkCheck()
+		testFlag = eb60AnkCheck(devId, appType, time=5, time_step=0.1)
 	if testFlag:
 		testFlag = eb60FindPoles()
 	if testFlag:
 		testFlag = eb60NoLoadAct()
 	if testFlag:
 		testFlag = eb60IMUCal()
+
 
 	if testFlag:
 		print("\nAll Tests Passed")
@@ -43,16 +44,16 @@ def eb60SensorCheck(devId, appType, time, time_step):
 
 	# Build dataDict for sensor data
 	dataDict = {}
-	data = getData(devId)
+	data = getData(devId, appType)
 	for d in data:
 		dataDict[d] = []
 
 	# Record data for time
 	totalLoopCount = int(time / time_step)
 	for i in range(totalLoopCount):
-		printLoopCount(i, totalLoopCount)
+		print(f'Sensor Check Measurement {i} of {totalLoopCount}')
 		sleep(time_step)
-		data = getData(devId)
+		data = getData(devId, appType)
 		for d in data:
 			dataDict[d].append(data[d])
 
@@ -60,14 +61,14 @@ def eb60SensorCheck(devId, appType, time, time_step):
 		print("Sensor Check Passed")
 		return True
 	else:
-		print("Sensor Check Failed")
+		print("Test Failed: Sensor Check Failed")
 		return False
 
 def checkSensVals(dataDict):
 	testPassed = True
 
 	# Check sensor are non zero
-	sensors = ['accelx', 'accely', 'accelz', 'gyrox', 'gyroy', 'gyroz', 'mot_ang', 'mot_cur', 'batt_volt', 'batt_curr', 'ank_ang', 'ank_vel']
+	sensors = ['accelx', 'accely', 'accelz', 'gyrox', 'gyroy', 'gyroz', 'mot_ang', 'mot_cur', 'batt_volt', 'batt_curr']
 	for sensor in sensors:
 		sensorMean = mean(dataDict[sensor])
 		if sensorMean == 0.0:
@@ -75,7 +76,7 @@ def checkSensVals(dataDict):
 			testPassed = False
 
 	# Check sensors are in range
-	sensors = ['accelx', 'accely', 'accelz', 'gyrox', 'gyroy', 'gyroz', 'batt_volt', 'batt_curr', 'temperature', 'ank_ang', 'ank_vel']
+	sensors = ['accelx', 'accely', 'accelz', 'gyrox', 'gyroy', 'gyroz', 'batt_volt', 'batt_curr', 'temperature']
 	sensorRange = {
 		'accelx': {'min': -25000, 'max': 25000},
 		'accely': {'min': -25000, 'max': 25000},
@@ -83,8 +84,8 @@ def checkSensVals(dataDict):
 		'gyrox': {'min': -15000, 'max': 15000},
 		'gyroy': {'min': -15000, 'max': 15000},
 		'gyroz': {'min': -15000, 'max': 15000},
-		'batt_volt': {'min': 190, 'max': 225},
-		'batt_curr': {'min': -40, 'max': 120},
+		'batt_volt': {'min': 180, 'max': 225},
+		'batt_curr': {'min': -130, 'max': 120},
 		'temperature': {'min': 10, 'max': 40},
 		'ank_ang': {'min': 2000, 'max': 6300},
 		'ank_vel': {'min': -500, 'max': 500}
@@ -93,82 +94,63 @@ def checkSensVals(dataDict):
 		sensorMin = min(dataDict[sensor])
 		sensorMax = max(dataDict[sensor])
 		if sensorMin < sensorRange[sensor]['min']:
-			print(f"Sensor Check Failed: {sensor} minimum is {sensorMin} (it should be above {sensorRange[sensor]['min']})")
+			print(f"\nSensor Check Failed: {sensor} minimum is {sensorMin} (it should be above {sensorRange[sensor]['min']})")
 			testPassed = False
 		if sensorMax > sensorRange[sensor]['max']:
-			print(f"Sensor Check Failed: {sensor} maximum is {sensorMax} (it should be above {sensorRange[sensor]['max']})")
+			print(f"\nSensor Check Failed: {sensor} maximum is {sensorMax} (it should be above {sensorRange[sensor]['max']})")
 			testPassed = False	
 
 	return testPassed
 
 # Ankle Encoder Check - Verfiy ankle encoder is functioning nominally
-def eb60AnkCheck(dataDict):
-		# Check ank angle
-	print('\n>>> User Input: Move ankle through its full range of travel\n')
-	# Check sensors are in range
-	sensors = ['ank_ang', 'ank_vel']
+def eb60AnkCheck(devId, appType, time, time_step):
+	print('\n>>> User Input: Move ankle through its full range of travel for 5 seconds\n')
+	sleep(3)
+
+	# Build dataDict for sensor data
+	dataDict = {}
+	data = getData(devId, appType)
+	for d in data:
+		dataDict[d] = []
+
+	# Record data for time
+	totalLoopCount = int(time / time_step)
+	for i in range(totalLoopCount):
+		print(f'Ankle Angle Check Measurement {i} of {totalLoopCount}')
+		sleep(time_step)
+		data = getData(devId, appType)
+		for d in data:
+			dataDict[d].append(data[d])
+
+	if checkAnkVals(dataDict):
+		print("Ankle Angle Check Passed")
+		return True
+	else:
+		print("Test Failed: Ankle Angle Check Failed")
+		return False		
+
+def checkAnkVals(dataDict):
+	# Check that sensors min and max are in range
+	testPassed = True
+
+	sensors = ['ank_ang']
 	sensorRange = {
-		'ank_ang': {'min': 2000, 'max': 6300},
-		'ank_vel': {'min': -500, 'max': 500}
+		'ank_ang': {'min_lo': 1990, 'min_hi': 2100, 'max_lo': 6200, 'max_hi': 6400},
 		}
 	for sensor in sensors:
 		sensorMin = min(dataDict[sensor])
 		sensorMax = max(dataDict[sensor])
-		if sensorMin < sensorRange[sensor]['min']:
-			print(f"Sensor Check Failed: {sensor} minimum is {sensorMin} (it should be above {sensorRange[sensor]['min']})")
+		if sensorMin < sensorRange[sensor]['min_lo'] or sensorMin > sensorRange[sensor]['min_hi']:
+			print(f"\nSensor Check Failed: {sensor} minimum is {sensorMin} (it should be between {sensorRange[sensor]['min_lo']} and {sensorRange[sensor]['min_hi']})")
 			testPassed = False
-		if sensorMax > sensorRange[sensor]['max']:
-			print(f"Sensor Check Failed: {sensor} maximum is {sensorMax} (it should be above {sensorRange[sensor]['max']})")
+		if sensorMax < sensorRange[sensor]['max_lo'] or sensorMax > sensorRange[sensor]['max_hi']:
+			print(f"\nSensor Check Failed: {sensor} maximum is {sensorMax} (it should be between {sensorRange[sensor]['max_lo']} and {sensorRange[sensor]['max_hi']})")
 			testPassed = False
 
 	return testPassed
 
 
-
-#########################################################
-# Generic functions
-
-def getData(devId):
-	return formatData(fxReadDevice(devId))
-
-def formatData(exoState: AllDevices.ExoState):
-	data = {
-	'state_time': exoState.state_time,
-	'accelx': exoState.accelx,
-	'accely': exoState.accely,
-	'accelz': exoState.accelz,
-	'gyrox': exoState.gyrox,
-	'gyroy': exoState.gyroy,
-	'gyroz': exoState.gyroz,
-	'mot_ang': exoState.mot_ang,
-	'mot_volt': exoState.mot_volt,
-	'mot_cur': exoState.mot_cur,
-	'batt_volt': exoState.batt_volt,
-	'batt_curr': exoState.batt_curr,
-	'temperature': exoState.temperature,
-	'genvar_0': exoState.genvar_0,
-	'genvar_1': exoState.genvar_1,
-	'genvar_2': exoState.genvar_2,
-	'genvar_3': exoState.genvar_3,
-	'genvar_4': exoState.genvar_4,
-	'genvar_5': exoState.genvar_5,
-	'genvar_6': exoState.genvar_6,
-	'genvar_7': exoState.genvar_7,
-	'genvar_8': exoState.genvar_8,
-	'genvar_9': exoState.genvar_9,
-	'ank_ang': exoState.ank_ang,
-	'ank_vel': exoState.ank_vel
-	}
-	return data
-	
-
 #####################################################################
-def eb60AnkCheck():
-	print("\nRotate Dummy Ankle")
-
-	print("Ank Check Passed")
-	return True
-
 def eb60FindPoles():
 	print("\nFinding Poles...")
 
